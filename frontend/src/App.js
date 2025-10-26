285afd971a9645408cec8c064766f78be60b937d
Melkzedk <melkywafula29@gmail.com>
2025-10-27 01:07:50 +0300

    small changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import EnhanceForm from './components/EnhanceForm';
 import './App.css';
+import { FaReact } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="container py-5">
-      <h1 className="mb-4 text-center">ClearShot — Image Enhancer</h1>
+      <h1 className="mb-4 text-center d-flex justify-content-center align-items-center gap-2">
+        <FaReact color="#61DBFB" size={40} />
+        ClearShot — Image Enhancer
+      </h1>
       <EnhanceForm />
     </div>
   );
