be2eeab40430f095676db920adbe4e5e2c2fc231
Melkzedk <melkywafula29@gmail.com>
2025-10-28 09:37:13 +0300

    updated server.js file

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,7 @@ app.post('/api/enhance', upload.single('image'), async (req, res) => {
 
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: 'Server error!!' });
   }
 });
 
