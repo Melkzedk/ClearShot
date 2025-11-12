de96d5f7e572019d5856c6f7a569e0cfc2d6e98e
Melkzedk <melkywafula29@gmail.com>
2025-11-12 08:12:03 +0300

    changes

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ app.post('/api/enhance', upload.single('image'), async (req, res) => {
       }
     );
 
-    // write buffer to the stream
+    // write buffer to the stream1
     uploadStream.end(req.file.buffer);
 
   } catch (err) {
